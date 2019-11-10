f8e581120ef17cd6384c5c382e12e40e84546041
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-10 00:18:44 -0300

    Buscando dados da API

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,5 +36,7 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     "react/state-in-constructor": 'off',
     "react/static-property-placement": 'off',
+    "eslint-disable-next-line": 'off',
+    "react/no-typos": 'off'
   },
 };
