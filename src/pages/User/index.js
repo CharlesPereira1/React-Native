f9c94bfb09bfed7011206598b9f8a73077776a51
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-19 23:17:58 -0300

    Desafio06 - Pull to Refresh

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -32,6 +32,7 @@ export default class User extends Component {
     stars: [],
     page: 1,
     loading: false,
+    refreshing: false,
   };
 
   async componentDidMount() {
@@ -53,6 +54,7 @@ export default class User extends Component {
       stars: page >= 2 ? [...stars, ...response.data] : response.data,
       page,
       loading: false,
+      refreshing: false,
     });
   };
 
@@ -64,9 +66,19 @@ export default class User extends Component {
     this.load(nextPage);
   };
 
+  refreshList = () => {
+    this.setState({ refreshing: true, stars: [] }, this.load);
+  };
+
+  handleNavigate = repository => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Repository', { repository });
+  };
+
   render() {
     const { navigation } = this.props;
-    const { stars, loading } = this.state;
+    const { stars, loading, refreshing } = this.state;
 
     const user = navigation.getParam('user');
 
@@ -82,6 +94,8 @@ export default class User extends Component {
         ) : (
           <Stars
             data={stars}
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
             onEndReachedThreshold={0.2}
             onEndReached={this.loadMore}
             keyExtractor={star => String(star.id)}
